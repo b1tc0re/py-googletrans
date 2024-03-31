055eb6d33e0a896eaba1c4eb3ffd04e5b20254f7
b1tc0re <b1tc0re@yandex.ru>
2024-03-31 21:26:38 +0300

    proxy verify

--- googletrans/client.py
+++ googletrans/client.py
@@ -55,7 +55,7 @@ class Translator:
                  timeout: Timeout = None,
                  http2=True):
 
-        self.client = httpx.Client(http2=http2)
+        self.client = httpx.Client(http2=http2, verify=false)
         if proxies is not None:  # pragma: nocover
             self.client.proxies = proxies
 
